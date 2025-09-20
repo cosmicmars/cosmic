35c229da5d1fe84601593e8c4b74a58a522ce5c1
Sasloveslo <klimanovstas57@gmail.com>
2025-09-21 01:41:15 +0300

    NotFixedPopUp

--- static/js/script.js
+++ static/js/script.js
@@ -247,4 +247,17 @@ document.addEventListener('DOMContentLoaded', function() {
                 icon: '🎮'
             }
                 ];
+const openPopUp = document.getElementById('open_pop_up')
+const closePopUp = document.getElementById('pop_up_close')
+const popUp = document.getElementById('pop_up')
+
+openPopUp.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
 
